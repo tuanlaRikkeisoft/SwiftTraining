0033710e20575693048425637fe947e15deac0a8
nguyenminhtien.hust@gmail.com <nguyenminhtien@Nguyens-Mac-mini.local>
2017-03-20 15:49:33 +0700

    start exam2

--- TrainingExe1/Exam2.swift
+++ TrainingExe1/Exam2.swift
@@ -10,17 +10,43 @@ import UIKit
 
 class Exam2: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var firstNameKataTextField: UITextField!
+    @IBOutlet weak var lastNameKataTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var dateOfBirthTextField: UITextField!
+    @IBOutlet weak var maleButton: UIButton!
+    @IBOutlet weak var feMaleButton: UIButton!
+
+    @IBAction func actionMale(_ sender: Any) {
+        
+    }
+    @IBAction func actionFeMail(_ sender: Any) {
+        
+    }
+    var firstName: String?
+    var lastName: String?
+    var firstNameKata: String?
+    var lastNameKata: String?
+    var emailKata: String?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
+    
+ 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-
+    func isValidEmail(testStr:String) -> Bool {
+        // print("validate calendar: \(testStr)")
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: testStr)
+    }
+    
 }

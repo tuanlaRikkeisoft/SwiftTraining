1d042ede2bb05f8f493d62441765f2464061d705
Nguyen Minh Tien <nguyenminhtien@nguyens-mac-mini.local>
2017-03-20 10:16:28 +0700

    Initial Commit

---
+++ TrainingExe1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrainingExe1
+//
+//  Created by Nguyen Minh Tien on 3/20/17.
+//  Copyright © 2017 Nguyen Minh Tien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
